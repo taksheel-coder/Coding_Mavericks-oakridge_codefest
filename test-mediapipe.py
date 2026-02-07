e668b3aab0103560436998c29dba7a7fc1c87d04
taksheel-coder <taksheelsubudhi@gmail.com>
2026-02-07 11:39:29 +0530

    dot person
    in this commit i have added dots in a proper structure which we can use in the future fo r face recognition

--- test-mediapipe.py
+++ test-mediapipe.py
@@ -1,34 +1,44 @@
+#commit 2
+
 import cv2
 import mediapipe as mp
 
-# intializing mp
-mp_face_detection = mp.solutions.face_detection
-mp_points = mp.solutions.drawing_utils
+# start face mesh
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
 
 webcam = cv2.VideoCapture(0)
 
-# selecting model
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+with mp_face_mesh.FaceMesh(
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
     while webcam.isOpened():
         works, frame = webcam.read()
         if not works:
-            print("none is there")
             continue
 
-        # convert bgr to rgb
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = face_detection.process(rgb)
+        results = face_mesh.process(rgb)
 
-        #face ui
-        if results.detections:
-            for detection in results.detections:
-                mp_points.draw_detection(frame, detection)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                #place in spot
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=None,  #no triangle connection
+                    landmark_drawing_spec=mp_drawing.DrawingSpec(
+                        color=(0, 255, 255),
+                        thickness=1,
+                        circle_radius=1
+                    )
+                )
 
-        cv2.imshow('MediaPipe Face Tracking', frame)
+        cv2.imshow('dot person', frame)
 
-        # Fail safe
         if cv2.waitKey(5) & 0xFF == ord('q'):
-            print("break")
             break
 
 webcam.release()
